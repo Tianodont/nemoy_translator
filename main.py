2560c18903faa4215e2fe1ac13e30261d449fd8f
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-02-08 21:57:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+import numpy 
+import json
+import tensorflow as tf
+import mediapipe as mp
+app = Flask(__name__)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(3375, 1)),
+  tf.keras.layers.Dense(3375, activation='relu'),
+  tf.keras.layers.Dense(3375, activation = 'relu'),
+  tf.keras.layers.Dense(8),
+])
+a=[]
+b=[]
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+lictus = ["Он.txt","Она.txt","Они.txt","С.txt","Транспорт.txt","Ты.txt","И.txt","Я.txt"]
+for i in range(8):
+  with open(lictus[i], 'r') as f:
+      for rtg in f:
+        for line in rtg.replace('"', "").replace("'", "").split("@"):
+          line = line.split("!")[:-1]
+          line = list(map(float, line))
+          
+          if line != []:
+            b.append(i)
+            t = numpy.asarray(line)
+            a.append(t)
+
+b = numpy.asarray(b)
+a = numpy.asarray(a)
+print(b, a)
+model.fit(
+    tf.stack(a),
+    tf.stack(b),
+    epochs=3,
+)
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        jsonData = request.get_json()["massive"][0]
+        print(len(jsonData))
+        array = numpy.asarray([jsonData])
+        predictions = model(array).numpy()
+        res = tf.nn.softmax(predictions).numpy()[0]
+        print(res)
+    return render_template('index.html')
+
+
+
+app.run(host='0.0.0.0', port=81)
+
